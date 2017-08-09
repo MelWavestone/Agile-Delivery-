1bff22577e9ff52f32b75d4a09b20b72e9f9c1cd
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-09 15:05:14 +0200

    Create calculator.test.js

---
+++ myapptest/test/calculator.test.js
@@ -0,0 +1,9 @@
+describe('test', function test_function() {
+
+                it('the page should display', function test_function() {
+                        expect(document.title).toEqual('My App');
+                        //var title = (title);
+                        //var sorted = title;
+                        //expect(sorted).toBe('My App');
+                });
+        });
