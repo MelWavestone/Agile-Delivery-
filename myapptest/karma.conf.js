18997ca50080a3853a6e283cb10e5cd922df7ed5
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-09 14:48:48 +0200

    Create karma.conf.js

---
+++ myapptest/karma.conf.js
@@ -0,0 +1,44 @@
+// Karma configuration
+// Generated on Fri Jun 30 2017 11:45:37 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'test/calculator.test.js',
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9090,
+
+
+    // enable / disable colors in the output (reporters and logs)
+                                                                       
